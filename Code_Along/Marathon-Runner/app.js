034b298b39563859f81046f0e5200450b02b04de
Michelle Yeung <michelle.yeung.080@gmail.com>
2016-01-16 16:47:28 -0800

    Reuploaded HW3

---
+++ Code_Along/Marathon-Runner/app.js
@@ -0,0 +1,9 @@
+var time = prompt("What was your best marathon time?");
+
+if(time > 6){
+  alert("You should speed up!");
+} else if(time < 4){
+  alert("Your time was excellent!");
+} else (time >= 4 && time <= 5);{
+  alert("Your time was average");
+}
