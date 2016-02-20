1b65a1affdd6c03ea79cc1d2e5f71726f7acca27
Michelle Yeung <michelle.yeung.080@gmail.com>
2016-02-20 05:36:14 -0800

    Fixed CSS for Final Project

--- Final-Project/apps.js
+++ Final-Project/apps.js
@@ -1,11 +1,9 @@
-$(document).ready(
-    function () {
-        $(".hoverli").hover(
-          function () {
-             $('ul.hover-menu').finish().slideDown('medium');
-          },
-          function () {
-             $('ul.hover-menu').finish().slideUp('medium');
-          }
-    );
-});
+$('nav li ul').hide().removeClass('hover-menu');
+$('nav li').hover(
+  function() {
+    $('ul', this).stop().slideDown(50);
+  },
+  function() {
+    $('ul', this).stop().slideUp(50);
+  }
+);
